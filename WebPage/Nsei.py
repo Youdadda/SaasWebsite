f9d996bad209ec1fbcf0de560c88db97bf1bc016
Youdadda <youssefdadda17@gmail.com>
2024-10-31 20:31:37 +0100

    Adding the image option

--- WebPage/Nsei.py
+++ WebPage/Nsei.py
@@ -13,14 +13,15 @@ data = add_indicators(data)
 latest_data = data.iloc[-1]
 model, FS , TS  = load_model_and_scalers("DeepAR/TraderO/deepar_model.h5", "DeepAR/TraderO/FS.save", "DeepAR/TraderO/TS.save")
 # Usage
-try:
+""" try:
     next_day_price = predict_next_day(model, FS, TS, latest_data)
     print(f"Predicted price for the next day: {next_day_price:.2f}")
 
     # Compare with the last known price
-    last_known_price = data['Close'].iloc[-1]
+    last_known_price = pd.DataFrame(data['Close'].iloc[-1])
     print(f"Last known price: {last_known_price:.2f}")
-    print(f"Predicted change: {next_day_price - last_known_price:.2f}")
-except Exception as e:
+    print(f"Predicted change: {next_day_price - last_known_price:.2f}") """
+Predict30days(model, FS, TS, latest_data)
+""" except Exception as e:
     print("Error occurred:", str(e))
-    print("latest_data:", latest_data)
+    print("latest_data:", latest_data) """
--- WebPage/app.py
+++ WebPage/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 import yfinance as yf
 from datetime import datetime, timedelta
 from DeepAR.ModelFunctionalities import *
@@ -53,11 +54,29 @@ def Transaltion():
     return render_template("FETranslation.html")
 
 
-@app.route("/Style")
+UPLOAD_FOLDER = './StyleTransfer/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):    
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/Style", methods = ["POST","GET"])
 def Style():
-    return render_template("StyleTransf.html")
-
-
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('StyleTransf.html')
 if __name__ == "__main__":
     app.run(debug=True)
 
